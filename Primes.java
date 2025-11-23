60788b755ba3c7788eea4213653e17393d7e3a38
Roi Refaeli <roi.refaeli@post.runi.ac.il>
2025-11-23 12:06:54 -0800

    Completed assignment

--- Primes.java
+++ Primes.java
@@ -1,7 +1,7 @@
 public class Primes {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        System.out.println("The prime numbers up to " + n + " :");
+        System.out.println("Prime numbers up to " + n + ":");
         boolean [] isPrime = new boolean[n + 1];
         int i = 2;
         while(i <= n) {
